6679e94b984a2d6c681bdb96f26ef1539be3eb94
Asuryu <tomas-250@outlook.com>
2021-12-14 00:06:12 +0000

    Fixe

--- cliente/cliente.c
+++ cliente/cliente.c
@@ -5,7 +5,14 @@
 #include "cliente.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <signal.h>
 #include <string.h>
+
+void handler_funcSignal(int signum){ // Para o sa_handler com (completo - PIDs, etc...)
+    printf("\nEstou vivo meu puto!\n");
+}
 
 int main(int argc, char *argv[]){
 
@@ -14,14 +21,22 @@ int main(int argc, char *argv[]){
     printf("|\\/||_ |  \\|/   /\\ |    (_ /  \\ \n");
     printf("|  ||__|__/|\\__/--\\|__  __)\\__/\n\n\n");
     char *mso_state = getenv("MEDICALSO_RUN");
-    if(mso_state == NULL){
-        printf("[MÉDICO]\nO balcão está fora de serviço\n");
-        return 0;
-    }
+    // if(mso_state == NULL){
+    //     printf("[MÉDICO]\nO balcão está fora de serviço\n");
+    //     return 0;
+    // }
     if(argv[1] == NULL) printf("[CLIENTE]\nPor favor insira um nome\nUtilização: ./cliente [nome]\n");
     else printf("[CLIENTE]\nBem vindo ao MEDICALso, %s\n", argv[1]);
-    
-    fflush(stdout);
+
+    struct sigaction sa;
+    sa.sa_handler = handler_funcSignal;
+    sa.sa_flags = SA_RESTART | SA_SIGINFO;
+    sigaction(SIGALRM, &sa, NULL);
+    // Criar alarme com o signal
+    while(1){
+        alarm(1);
+        pause();
+    }
 
     return 0;
 }
